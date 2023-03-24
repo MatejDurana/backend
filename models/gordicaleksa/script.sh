05b1df13b1e9416f7e8c870690f60da298362c79
MatejDurana <79214120+MatejDurana@users.noreply.github.com>
2023-03-24 20:06:52 +0100

    Update script.sh

--- models/gordicaleksa/script.sh
+++ models/gordicaleksa/script.sh
@@ -1,4 +1,4 @@
 params=$1
-. models/gordicaleksa/py_env/venv/bin/activate
+. models/gordicaleksa/gordicaleksa_env/bin/activate
 python models/gordicaleksa/neural_style_transfer.py  --content_img_name content_image.jpg --style_img_name style_image.jpg $params
 deactivate
