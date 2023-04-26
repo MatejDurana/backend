e6cc0d592913dd4fb0f111edfcb0103f988b78a8
MatejDurana <79214120+MatejDurana@users.noreply.github.com>
2023-04-26 21:22:47 +0200

    Update script.sh

--- models/zhanghang/script.sh
+++ models/zhanghang/script.sh
@@ -1,3 +1,3 @@
 . models/zhanghang/zhanghang_env/bin/activate
-python3.6 models/zhanghang/experiments/main.py eval --content-image images/content_image.jpg --style-image images/style_image.jpg --output-image images/output.jpg  --model models/zhanghang/experiments/models/21styles.model --cuda=0
+python3.10 models/zhanghang/experiments/main.py eval --content-image images/content_image.jpg --style-image images/style_image.jpg --output-image images/output.jpg  --model models/zhanghang/experiments/models/21styles.model --cuda=0
 deactivate
